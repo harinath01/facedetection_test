f1e6f54191e29fe92937ebcfb4eec964ce09e3ce
hari-testpress <hari@testpress.in>
2025-11-10 17:47:59 +0530

    Update minimum detection confidence to 0.7 in face detection configuration

--- src/main.ts
+++ src/main.ts
@@ -58,7 +58,7 @@ class FaceDetectionApp {
                 modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
                 delegate: "GPU"
             },
-            minDetectionConfidence: 0.8,
+            minDetectionConfidence: 0.7,
             runningMode: "IMAGE"
         });
     }
