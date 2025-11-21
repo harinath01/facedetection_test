635dfbd3f02067ff97715317e8e1f05aaba5e582
hari-testpress <hari@testpress.in>
2025-11-21 12:33:59 +0530

    Update Mediapipe vision URL to a static resource for improved reliability

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {
 
 
 const MEDIAPIPE_VISION_URL =
-  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.10/wasm";
+  "https://static.testpress.in/static/mediapipe/tasks-vision/wasm";
 const MEDIAPIPE_MODEL_URL =
   "https://static.testpress.in/static/mediapipe/models/blaze_face_short_range.tflite";
 
